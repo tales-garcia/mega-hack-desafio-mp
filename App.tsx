a459bb4276586ea911ce4c978d95fe91bcfc0818
Tales Garcia <garcia.tales@gmail.com>
2020-11-12 14:52:47 -0300

    build(whole app): app setup
    Prepare app for development

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import {
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <View style={styles.header}>
+          <View style={styles.topContent}>
+            <View>
+              <Text>Olá Tales</Text>
+            </View>
+            <View>
+              <Feather name="bell" color="#fff" size={20} />
+            </View>
+          </View>
+        </View>
+        <View>
+
+        </View>
+        <View>
+
+        </View>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  header: {
+    backgroundColor: '#009EE3',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    width: Dimensions.get('window').width,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    paddingVertical: 38,
+  },
+  topContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
   },
 });
+
+export default App;
